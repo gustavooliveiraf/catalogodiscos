76515168d2ffee99fd0ad305ddd1d05fd1237437
Gustavo Oliveira <gof@cin.ufpe.br>
2019-01-07 01:32:02 -0300

    redirecionando para home

--- src/components/shared/menu/Menu.vue
+++ src/components/shared/menu/Menu.vue
@@ -1,6 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Catálogo de discos</a>
+    <router-link :to="{ name: 'home' }"> <a class="navbar-brand" href="#">Catálogo de discos</a> </router-link>
+    
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
